3ca3bf6edc7663d487d96f218dd169d9f2744744
AsrulDeniarahman <64624166+AsrulDeniarahman@users.noreply.github.com>
2020-05-14 22:47:38 -0700

    Add files via upload
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int my array[8] = {1,2,4,8,16,32,64,128};
+	printf("data ke-1 = %d", my array[0]);
+	printf("data ke-2 = %d", my array[1]);
+	printf("data ke-3 = %d", my array[2]);
+	printf("data ke-4 = %d", my array[3]);
+	printf("data ke-5 = %d", my array[4]);
+	printf("data ke-6 = %d", my array[5]);
+	printf("data ke-7 = %d", my array[6]);
+	printf("data ke-8 = %d", my array[7]);
+}
