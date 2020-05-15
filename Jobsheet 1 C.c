bb93b525d00d3fa8daf093d295946bed6e5f36e9
AsrulDeniarahman <64624166+AsrulDeniarahman@users.noreply.github.com>
2020-05-14 22:38:57 -0700

    Add files via upload
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int angka[5] = {1,9,2,8};
+	printf("Nama : Asrul Deniarahman");
+	printf("\nNim : F1B019028");
+	printf("\nKelompok : 7\n");
+	
+	printf("\nData ke-1= %d",angka[0]);
+	printf("\nData ke-2= %d",angka[1]);
+	printf("\nData ke-3= %d",angka[2]);
+	printf("\nData ke-4= %d",angka[3]);
+	printf("\nData ke-5= %d",angka[4]);
+}
+
