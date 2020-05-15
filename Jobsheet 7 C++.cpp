505f831b950332fd3c9838507adaae9656d4732b
AsrulDeniarahman <64624166+AsrulDeniarahman@users.noreply.github.com>
2020-05-14 22:55:57 -0700

    Add files via upload
    Mengisi elemen array menggunakan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Asrul Deniarahman_F1B019028\n";
+  int x[5],i;
+  for(i=0;i<5;i++){
+  	cout<<"Masukan Nilai "<<i+1<<" : ";
+  	cin>>x[i];
+  }
+  return 0;
+}
+
